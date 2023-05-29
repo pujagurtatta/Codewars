0ec44c36b7418d83993037f7a462fb1c71272095
PujaGurtatta <workforpuja.social@gmail.com>
2023-05-30 02:31:47 +0530

    New Codewar Challenge Added

---
+++ 8kyu/keep-hydrated.js
@@ -0,0 +1,19 @@
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling. 
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+
+// MY SOLUTION
+
+function litres(time){
+    let noOfLitres = time * 0.5;
+    return noOfLitres
+}
+
+// another solution
+
+function litres(time){
+    return time * 0.5
+}
