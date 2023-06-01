617b74a4b0c3f8c9bd9f7f1b03cfece74930d2b3
PujaGurtatta <workforpuja.social@gmail.com>
2023-06-01 10:53:32 +0530

    New Codewar Challenge Added

---
+++ 8kyu/unusual-five.js
@@ -0,0 +1,14 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+
+// SOLUTION 
+
+function unusualFive(){
+    return 'ppppp'.length
+}
+
+// SOLUTION 2
+
+const unusualFive = () => 'What are you doing here?'.split(' ').length
