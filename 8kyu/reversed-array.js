f6becec6e00d1ee14bf471961a4eead8f8ab2deb
PujaGurtatta <workforpuja.social@gmail.com>
2023-06-03 13:44:06 +0530

    New Codewar Challenge Added

---
+++ 8kyu/reversed-array.js
@@ -0,0 +1,7 @@
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+
+function digitize(n){
+    return n.toString().split('').reverse().map(Number)
+}
