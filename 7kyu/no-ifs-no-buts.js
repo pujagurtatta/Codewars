8efdf27543b930a6b666f4e0ace957be37462f26
PujaGurtatta <workforpuja.social@gmail.com>
2023-06-06 18:46:27 +0530

    New Codewar Challenge Added

---
+++ 7kyu/no-ifs-no-buts.js
@@ -0,0 +1,41 @@
+// Write a function that accepts two numbers a and b and returns whether a is smaller than, bigger than, or equal to b, as a string.
+
+// (5, 4)   ---> "5 is greater than 4"
+// (-4, -7) ---> "-4 is greater than -7"
+// (2, 2)   ---> "2 is equal to 2"
+// There is only one problem...
+
+// You cannot use if statements, and you cannot use the ternary operator ? :.
+
+// In fact the word if and the character ? are not allowed in your code.
+
+
+//SOLUTION 1
+
+let noIfsNoButs = function(a, b){
+    switch(true){
+        case(a < b) : return a + "is smaller than" + b;
+        case(a > b) : return a + "is greater than" + b;
+        default     : return a + "is equal to"     + b;
+    }
+}
+
+// SOLUTION 2
+
+let noIfsNoButs = function(a,b){
+    while(a > b) return a + "is greater than" + b;
+    while(a < b) return a + "is smaller than" + b;
+    return a + "is equal to" + b;
+}
+
+// SOLUTION 3
+
+const noIfsNoButs = function(a,b){
+
+    while( a > b )
+        return `${a} is greater than ${b}`
+    while( a < b ) 
+        return `${a} is smaller than ${b}`;
+    while( a == b )
+        return `${a} is equal to ${b}`;
+}
