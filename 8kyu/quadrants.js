185cd30a9b008cd66f7795b4a6e95c21cd356cf3
PujaGurtatta <workforpuja.social@gmail.com>
2023-06-17 10:11:14 +0530

    New Codewar Challenge Added

---
+++ 8kyu/quadrants.js
@@ -0,0 +1,17 @@
+// Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.
+
+function quadrants(x, y){
+    if(x > 0 && y > 0){
+        return 1
+    }else if (x < 0 && y > 0){
+        return 2
+    }else if (x < 0 && y < 0){
+        return 3
+    }else if(x > 0 && y < 0){
+        return 4
+    }
+}
+
+function quadrant(x,y){
+    return x > 0 ? y > 0 ? 1 : 4 : y > 0 ? 2 : 3;
+}
